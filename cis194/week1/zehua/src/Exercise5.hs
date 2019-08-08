ffc8c9877819135fc053e57abfcfda4be08b6bb7
Zehua Liu <zehua@canva.com>
2019-08-08 11:35:18 +1000

    PR comment - formatting

--- cis194/week1/zehua/src/Exercise5.hs
+++ cis194/week1/zehua/src/Exercise5.hs
@@ -6,5 +6,7 @@ type Move = (Peg, Peg)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _ _ _ = []
 hanoi 1 a b _ = [(a, b)]
-hanoi n a b c = hanoi n1 a c b ++ [(a, b)] ++ hanoi n1 c b a
-                where n1 = n - 1
+hanoi n a b c =
+    hanoi n1 a c b ++ [(a, b)] ++ hanoi n1 c b a
+  where
+    n1 = n - 1
