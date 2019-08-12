00f516f4862c3e504764dcb07dce1352bb98fdd9
Daniel Deng <sinogermany@gmail.com>
2019-08-13 09:54:28 +1000

    Daniel Deng - Week 01 - Hanoi 3 Pegs (#5)
    * Daniel Deng - Week 01 - Hanoi 3 Pegs
    
    * Daniel Deng - Week 01 - Hanoi 3 Pegs - Rev01
    
    * Daniel Deng - Week 01 - Hanoi 3 Pegs - Rev02
    
    * Daniel Deng - Week 01 - Hanoi 3 Pegs - Rev03

---
+++ cis194/week1/daniel-deng/src/CIS194/Homework01/Exercise05.hs
@@ -0,0 +1,20 @@
+module CIS194.Homework01.Exercise05 (hanoi) where
+
+type Disks = Integer
+type Peg   = String
+type Move  = (Peg, Peg)
+
+{-
+  To move n discs (stacked in increasing size) from peg a to peg b
+  using peg c as temporary storage,
+  1. move n − 1 discs from a to c using b as temporary storage
+  2. move the top disc from a to b
+  3. move n − 1 discs from c to b using a as temporary storage.
+-}
+hanoi :: Disks -> Peg -> Peg -> Peg -> [Move]
+hanoi n start destination temp
+  | 0 >= n    = []
+  | otherwise = hanoi n' start temp destination
+                ++ [(start, destination)]
+                ++ hanoi n' temp destination start
+              where n' = n - 1
---
+++ cis194/week1/daniel-deng/test/CIS194/Homework01/Exercise05Spec.hs
@@ -0,0 +1,24 @@
+module CIS194.Homework01.Exercise05Spec where
+
+import CIS194.Homework01.Exercise05
+
+import Test.Tasty.Hspec
+import Control.Exception (evaluate)
+
+spec_hanoi :: Spec
+spec_hanoi = do
+
+  it "hanoi with negative disks should take no move" $
+    hanoi (-1) "a" "b" "c" `shouldBe` []
+
+  it "hanoi with 0 disks should take no move" $
+    hanoi 0 "a" "b" "c" `shouldBe` []
+
+  it "hanoi with 1 disks should take 1 move" $
+    hanoi 1 "a" "b" "c" `shouldBe` [("a", "b")]
+
+  it "hanoi with 2 disks should take 3 moves" $
+    hanoi 2 "a" "b" "c" `shouldBe` [("a","c"), ("a","b"), ("c","b")]
+
+  it "hanoi with 3 disks should take 7 moves" $
+    hanoi 3 "a" "b" "c" `shouldBe` [("a","b"),("a","c"),("b","c"),("a","b"),("c","a"),("c","b"),("a","b")]
