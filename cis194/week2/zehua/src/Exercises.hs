a0b3bc00fce6d74290326106cfb340ebf4bc47b8
Zehua Liu <zehua.liu@gmail.com>
2019-08-19 13:39:56 +1000

    rest of week 2 exercises (#31)
    * ex 2
    
    * add other ex
    
    * refactor test
    
    * simplify findEventsBefore
    
    * fix sort
    
    * formatting
    
    * PR comments
    
    * PR comment - catMaybes instead of fromJust to avoid runtiem error
    
    * PR comment - use mapMaybe instead of catMaybes
    
    * PR comment - move sortLogMessages into ex4 test

--- cis194/week2/zehua/src/Exercises.hs
+++ cis194/week2/zehua/src/Exercises.hs
@@ -1,7 +1,8 @@
 module Exercises where
 
-import Text.Read
-import Log
+import           Data.Maybe (mapMaybe)
+import           Log
+import           Text.Read  (readMaybe)
 
 -- ex1
 parseInt :: String -> Maybe TimeStamp
@@ -10,7 +11,7 @@ parseInt = readMaybe
 createLogMessage :: MessageType -> String -> [String] -> Maybe LogMessage
 createLogMessage mt ts msg =
   case parseInt ts of
-    Just t -> Just . LogMessage mt t . unwords $ msg
+    Just t  -> Just . LogMessage mt t . unwords $ msg
     Nothing -> Nothing
 
 parseWords :: [String] -> Maybe LogMessage
@@ -31,3 +32,51 @@ parseMessage s =
 
 parse :: String -> [LogMessage]
 parse = fmap parseMessage . lines
+
+-- ex2
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) t = t
+insert lm Leaf       = Node Leaf lm Leaf
+insert lm@(LogMessage _ ts _ ) (Node l lm1@(LogMessage _ ts1 _) r)
+  | ts < ts1 = Node (insert lm l) lm1 r
+  | otherwise = Node l lm1 (insert lm r)
+insert _ t           = t
+
+-- ex3
+build :: [LogMessage] -> MessageTree
+build = foldr insert Leaf
+
+-- ex4
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf          = []
+inOrder (Node l lm r) = inOrder l ++ [lm] ++ inOrder r
+
+-- ex5
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong = mapMaybe getMessageMaybe . filter error50 . inOrder . build
+  where
+    error50 (LogMessage (Error s) _ _) = s >= 50
+    error50 _                          = False
+    getMessageMaybe (LogMessage _ _ m) = Just m
+    getMessageMaybe _                  = Nothing
+
+-- ex6
+-- sort events and find the events happened before a particular log message
+findEventsBefore :: String -> [LogMessage] -> [LogMessage]
+findEventsBefore event = takeWhile (notseen event) . filter known . inOrder . build
+  where
+    notseen e (LogMessage _ _ m) = e /= m
+    notseen _ _                  = False
+    known (Unknown _) = False
+    known _           = True
+
+findEventsBeforeFromFile :: String -> String -> IO [LogMessage]
+findEventsBeforeFromFile event file = findEventsBefore event . parse <$> readFile file
+
+-- To use it, run `stack ghci`.
+-- from ex5, we run the following to get the list of severe errors:
+--  testWhatWentWrong parse whatWentWrong  "testdata/error.log" >>= mapM_ putStrLn
+-- to print the list of events right before the first severe error:
+--  fmap show <$> findEventsBeforeFromFile "Mustardwatch opened, please close for proper functioning!" "testdata/error.log" >>= mapM_ putStrLn
+-- It looks like Alice (the girl in the wonderland) was being naughty and
+-- touched the jar to open the mustardwatch?
--- cis194/week2/zehua/test/ExercisesSpec.hs
+++ cis194/week2/zehua/test/ExercisesSpec.hs
@@ -1,8 +1,21 @@
 module ExercisesSpec where
 
-import Test.Hspec
-import Exercises
-import Log
+import           Data.List  (sort)
+import           Exercises
+import           Log
+import           Test.Hspec
+
+-- util to make LogMessage sortable.
+-- Using `newtype` to avoid having to touch the original def in Log.hs
+newtype SortableLogMessage = SortableLogMessage { getLogMessage :: LogMessage } deriving (Eq, Show)
+instance Ord SortableLogMessage where
+  a1 <= a2 = let b1 = getLogMessage a1
+                 b2 = getLogMessage a2
+             in b1 `lteq` b2
+               where
+                 Unknown _ `lteq` _         = True
+                 _ `lteq` Unknown _         = False
+                 LogMessage _ ts1 _ `lteq` LogMessage _ ts2 _ = ts1 <= ts2
 
 spec :: Spec
 spec = do
@@ -46,7 +59,7 @@ spec = do
         LogMessage (Error 20) 2 "Too many pickles",
         LogMessage Info 9 "Back from lunch",
         LogMessage (Error 99) 10 "Flange failed!"
-        ] 
+        ]
 
     it "works with first 5 lines of error.log" $ do
       actual <- testParse parse 5 "testdata/error.log"
@@ -57,3 +70,138 @@ spec = do
         LogMessage Info 4076 "verse.'",
         LogMessage Info 4764 "He trusts to you to set them free,"
         ]
+
+  describe "ex2" $ do
+    it "returns original when given unknown" $ do
+      insert (Unknown "unknown msg") Leaf `shouldBe` Leaf
+      insert (Unknown "unknown msg") (Node Leaf (LogMessage Info 123 "info") Leaf)
+        `shouldBe` (Node Leaf (LogMessage Info 123 "info") Leaf)
+
+    it "handles leaf node" $ do
+      insert (LogMessage Warning 123 "warn") Leaf `shouldBe`
+        (Node Leaf (LogMessage Warning 123 "warn") Leaf)
+
+    it "inserts to left when smaller" $ do
+      insert (LogMessage Warning 123 "warn") (Node Leaf (LogMessage Info 124 "info") Leaf)`shouldBe`
+        (Node (Node Leaf (LogMessage Warning 123 "warn") Leaf) (LogMessage Info 124 "info") Leaf)
+
+    it "inserts to right when equal or greater" $ do
+      insert (LogMessage Warning 123 "warn") (Node Leaf (LogMessage Info 122 "info") Leaf)`shouldBe`
+        (Node Leaf (LogMessage Info 122 "info") (Node Leaf (LogMessage Warning 123 "warn") Leaf))
+
+      insert (LogMessage Warning 123 "warn") (Node Leaf (LogMessage Info 123 "info") Leaf)`shouldBe`
+        (Node Leaf (LogMessage Info 123 "info") (Node Leaf (LogMessage Warning 123 "warn") Leaf))
+
+  describe "ex3" $ do
+    it "returns leaf for empty list" $ do
+      build [] `shouldBe` Leaf
+
+    it "handles list" $ do
+      build
+          [ (LogMessage Warning 124 "third")
+          , (LogMessage Info 122 "first")
+          , (LogMessage (Error 1) 123 "second")
+          , (LogMessage (Error 2) 125 "fourth") ]
+        `shouldBe`
+          Node
+            (Node
+              (Node Leaf (LogMessage Info 122 "first") Leaf)
+              (LogMessage (Error 1) 123 "second")
+              (Node Leaf (LogMessage Warning 124 "third") Leaf))
+            (LogMessage (Error 2) 125 "fourth")
+            Leaf
+
+  describe "ex4" $ do
+    it "returns empty for a leaf" $ do
+      inOrder Leaf `shouldBe` []
+
+    it "handles node" $ do
+      inOrder (Node
+                Leaf
+                (LogMessage Info 122 "first")
+                (Node
+                  (Node Leaf (LogMessage (Error 1) 123 "second") Leaf)
+                  (LogMessage Warning 124 "third")
+                  (Node Leaf (LogMessage (Error 2) 125 "fourth") Leaf)))
+        `shouldBe` [ (LogMessage Info 122 "first")
+                   , (LogMessage (Error 1) 123 "second")
+                   , (LogMessage Warning 124 "third")
+                   , (LogMessage (Error 2) 125 "fourth") ]
+
+    it "combines with build should be equivalent to sort without Unknown" $ do
+      let input  =
+              [ (LogMessage Warning 124 "third")
+              , (LogMessage Info 122 "first")
+              , (LogMessage (Error 1) 123 "second")
+              , (LogMessage (Error 2) 125 "fourth") ]
+          sortLogMessages :: [LogMessage] -> [LogMessage]
+          sortLogMessages = fmap getLogMessage . sort . fmap SortableLogMessage
+      (inOrder . build) input `shouldBe` sortLogMessages input
+
+  describe "ex5" $ do
+    it "handles empty list" $ do
+      whatWentWrong [] `shouldBe` []
+
+    it "filters not qualified logs" $ do
+      whatWentWrong [ (LogMessage Warning 122 "warn") ] `shouldBe` []
+      whatWentWrong [ (LogMessage Info 122 "info") ] `shouldBe` []
+      whatWentWrong [ (LogMessage (Error 49) 122 "error 49") ] `shouldBe` []
+
+    it "keeps logs when error >= 50" $ do
+      whatWentWrong
+          [ (LogMessage Warning 122 "warn")
+          , (LogMessage (Error 50) 122 "error 50")
+          , (LogMessage Info 122 "info")
+          , (LogMessage (Error 49) 122 "error 49") ]
+        `shouldBe` [ "error 50" ]
+      whatWentWrong
+          [ (LogMessage Warning 121 "warn")
+          , (LogMessage (Error 51) 122 "area 51")
+          , (LogMessage Info 120 "info")
+          , (LogMessage (Error 49) 123 "error 49") ]
+        `shouldBe` [ "area 51" ]
+
+    it "filters and sorts logs when error >= 50" $ do
+      whatWentWrong
+          [ (LogMessage (Error 100) 123 "error 100")
+          , (LogMessage Warning 122 "warn")
+          , (LogMessage (Error 51) 122 "area 51")
+          , (LogMessage Info 122 "info")
+          , (LogMessage (Error 99) 125 "error 99")
+          , (LogMessage (Error 49) 122 "error 49")
+          , (LogMessage (Error 51) 124 "error 75") ]
+        `shouldBe`
+          [ "area 51"
+          , "error 100"
+          , "error 75"
+          , "error 99" ]
+
+    it "parses from sample.log" $ do
+      s <- testWhatWentWrong parse whatWentWrong "testdata/sample.log"
+      s `shouldBe`
+        [ "Way too many pickles"
+        , "Bad pickle-flange interaction detected"
+        , "Flange failed!" ]
+    it "parses from error.log" $ do
+      s <- testWhatWentWrong parse whatWentWrong "testdata/error.log"
+      take 5 s `shouldBe`
+        [ "Mustardwatch opened, please close for proper functioning!"
+        , "All backup mustardwatches are busy"
+        , "Depletion of mustard stores detected!"
+        , "Hard drive failure: insufficient mustard"
+        , "All backup mustardwatches are busy" ]
+
+  describe "ex6" $ do
+    it "returns empty when event is the first one" $ do
+      findEventsBefore "warn" [ (LogMessage Warning 122 "warn") ] `shouldBe` []
+
+    it "returns when event is the last" $ do
+      findEventsBefore "error 49"
+          [ (LogMessage Warning 123 "warn")
+          , (LogMessage (Error 50) 124 "error 50")
+          , (LogMessage (Error 49) 125 "error 49")
+          , (LogMessage Info 122 "info") ]
+        `shouldBe`
+          [ (LogMessage Info 122 "info")
+          , (LogMessage Warning 123 "warn")
+          , (LogMessage (Error 50) 124 "error 50") ]
