8bf32c9c5a589203696341fd000ee0db5efb62d2
Steve Mao <maochenyan@gmail.com>
2019-08-21 09:44:14 +1000

    exercise 2 (#35)
    * exercise 2
    
    * exercise 3
    
    * Exercise 4
    
    * Exercise 5
    
    * naming
    
    * naming
    
    * sort
    
    * make compiler happy
    
    * comment
    
    * improve
    
    * remove comment
    
    * use lambda expression
    
    * use foldr
    
    * recursion solution
    
    * newline

--- cis194/week2/stevemao/LogAnalysis.hs
+++ cis194/week2/stevemao/LogAnalysis.hs
@@ -12,7 +12,6 @@ process a b d c = maybe (Unknown a) (\ts -> LogMessage c ts (unwords b)) (rmi d)
 processError :: String -> [String] -> String -> String -> LogMessage
 processError a b c d = maybe (Unknown a) (process a b d . Error) (rmi c)
 
--- "I 29 la la la"
 parseMessage :: String -> LogMessage
 parseMessage a = case words a of
   ("I":x:y) -> process a y x Info
@@ -22,3 +21,27 @@ parseMessage a = case words a of
 
 parse :: String -> [LogMessage]
 parse = fmap parseMessage . lines
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert a Leaf = Node Leaf a Leaf
+insert log1@(LogMessage _ ts1 _) (Node left log2@(LogMessage _ ts2 _) right)
+  | ts1 > ts2 = Node left log2 (insert log1 right)
+  | otherwise = Node (insert log1 left) log2 right
+insert _ tree = tree
+
+build :: [LogMessage] -> MessageTree
+build = foldr insert Leaf
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left a right) = inOrder left ++ [a] ++ inOrder right
+
+whatWentWrongNotSorted :: [LogMessage] -> [String]
+whatWentWrongNotSorted [] = []
+whatWentWrongNotSorted (LogMessage (Error severity) _ message: logMessages)
+  | severity > 50 = [message] ++ whatWentWrong logMessages
+  | otherwise = whatWentWrong logMessages
+whatWentWrongNotSorted (_: logMessages) = whatWentWrong logMessages
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong = whatWentWrongNotSorted . inOrder . build
