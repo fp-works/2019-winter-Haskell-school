65437398ae6a40215bad3c6dc862aa18eaf46bea
Robert Chang <cht8687@gmail.com>
2019-08-24 23:45:29 +1000

    Tasks2, Task3, Task4, Task5 (#32)
    * [Homework01-Task4]:validate
    
    * Week2-Task1
    
    * update
    
    * ViewPattern
    
    * update readme
    
    * Task2,Task3
    
    * when equal
    
    * Address PR review
    
    * refactored
    
    * Task 4
    
    * Refactor
    
    * Task 5
    
    * Update cis194/week2/robert-chang/src/LogAnalysis.hs
    
    Co-Authored-By: Daniel Deng <sinogermany@gmail.com>
    
    * Update cis194/week2/robert-chang/src/LogAnalysis.hs
    
    Co-Authored-By: Daniel Deng <sinogermany@gmail.com>
    
    * Update cis194/week2/robert-chang/src/LogAnalysis.hs
    
    Co-Authored-By: Shine Li <fcpga_te@icloud.com>

--- cis194/week2/robert-chang/src/LogAnalysis.hs
+++ cis194/week2/robert-chang/src/LogAnalysis.hs
@@ -53,3 +53,48 @@ parse :: String -> [LogMessage]
 parse [] = []
 parse file = map parseMessage $ lines file
 
+
+
+-- Exercise 2
+
+insert :: LogMessage -> MessageTree -> MessageTree
+{- if insert is given a LogMessage which is
+Unknown, it should return the MessageTree unchanged
+-}
+{- insert to empty tree, should have LogMessage inserted -} 
+insert x Leaf        = Node Leaf x Leaf 
+insert x@(LogMessage _ t1 _) (Node left xs@(LogMessage _ t2 _) right) 
+  | t1 == t2 = Node left xs right
+  | t1 < t2  = Node (insert x left) xs right
+  | t1 > t2  = Node left xs (insert x right)
+
+insert _ tree = tree
+
+-- Exercise 3
+build :: [LogMessage] -> MessageTree
+-- foldr :: (a -> b -> b) -> b -> t a -> b
+-- e.g.
+-- foldr f z []     = z 
+-- foldr f z (x:xs) = f x (foldr f z xs)
+-- In this case:
+-- foldr insert Leaf [x1, x2, ..., xn] == x1 `insert` (x2 `insert` ... (xn `insert` Leaf)...)
+build = foldr insert Leaf 
+
+-- Exercise 4
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left x right) = inOrder left ++ [x] ++ inOrder right
+
+-- Exercise 5
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong =  fmap getMsg . inOrder . build . filter . severity $ 50
+
+severity :: Int -> LogMessage -> Bool
+severity level (LogMessage (Error lvl) _ _)
+  | lvl >= level = True
+  | otherwise = False
+severity _ _ = False
+
+getMsg :: LogMessage -> String
+getMsg (LogMessage (Error _) _ msg) = msg
+getMsg _ = undefined
--- cis194/week2/robert-chang/test/LibSpec.hs
+++ cis194/week2/robert-chang/test/LibSpec.hs
@@ -5,8 +5,16 @@ import Log
 import Test.Hspec
 
 spec :: Spec
-spec = do
-  describe "parseMessage" $ do
+spec =
+  describe "LogAnalysis" $ do
+   parseMessageSpec
+   insertSpec
+   buildSpec
+   inOrderSpec
+   whatWentWrongSpec
+
+parseMessageSpec :: Spec
+parseMessageSpec = do
    it "returns correct Error Level " $ do
       parseMessage "E 2 562 help help" `shouldBe` LogMessage (Error 2) 562 "help help"
    it "return correct Info Level " $ do
@@ -16,8 +24,33 @@ spec = do
    it "return correct unKnown" $ do
       parseMessage "haha Steve Mao" `shouldBe` Unknown "haha Steve Mao"
 
-  -- I don't know how to test this guy
-  -- describe "parse" $ do
-  --  it "returns correct result" $ do   
-     -- testParse parse 1 "test/sample.log" `shouldBe` LogMessage Info 6 "Completed armadillo processing"
-    
+insertSpec :: Spec
+insertSpec = do
+  let tree = Node (Node Leaf (LogMessage Info 29 "la la la") Leaf) (LogMessage Warning 29 "la la la") Leaf 
+  it "should insert LogMessage into the tree" $
+    insert (LogMessage (Error 2) 562 "help help") Leaf `shouldBe` Node Leaf (LogMessage (Error 2) 562 "help help") Leaf
+
+  it "should insert LogMessage into the left tree when timestamp is smaller" $
+    insert (LogMessage (Error 2) 10 "help help") tree `shouldBe` Node (Node (Node Leaf (LogMessage (Error 2) 10 "help help") Leaf) (LogMessage Info 29 "la la la") Leaf) (LogMessage Warning 29 "la la la") Leaf 
+
+  it "should insert LogMessage into the right tree when timestamp is bigger" $
+    insert (LogMessage (Error 2) 590 "help help") tree `shouldBe` Node (Node Leaf (LogMessage Info 29 "la la la") Leaf) (LogMessage Warning 29 "la la la") (Node Leaf (LogMessage (Error 2) 590 "help help") Leaf)
+
+
+buildSpec :: Spec
+buildSpec = do
+  let list = [ (LogMessage (Error 2) 590 "help help"), (LogMessage Info 29 "la la la") ];
+  it "should build the logMessage" $
+      build list `shouldBe` Node Leaf (LogMessage Info 29 "la la la") (Node Leaf (LogMessage (Error 2) 590 "help help") Leaf)
+
+inOrderSpec :: Spec
+inOrderSpec = do 
+  let tree = Node (Node Leaf (LogMessage Info 29 "la la la") Leaf) (LogMessage Warning 29 "la la la") Leaf 
+  it "should build the message list" $
+    inOrder tree `shouldBe` [LogMessage Info 29 "la la la",  LogMessage Warning 29 "la la la"]
+
+whatWentWrongSpec :: Spec
+whatWentWrongSpec = do
+  let list = [ (LogMessage (Error 82) 590 "help help"), (LogMessage Info 29 "la la la"), (LogMessage (Error 83) 591 "help help2"), (LogMessage (Error 10) 590 "help help3") ];
+  it "should return correct message" $
+    whatWentWrong list `shouldBe` ["help help", "help help2"] 
