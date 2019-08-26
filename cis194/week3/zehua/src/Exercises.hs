b69387bf30c80331bd9b4ab03214216f7886514f
Zehua Liu <zehua.liu@gmail.com>
2019-08-26 23:23:08 +1000

    improved ex1 wk3 with list applicative (#44)

--- cis194/week3/zehua/src/Exercises.hs
+++ cis194/week3/zehua/src/Exercises.hs
@@ -1,10 +1,13 @@
 module Exercises where
 
+import           Control.Applicative (liftA2)
 import           Control.Monad (ap)
 import           Data.Bool (bool)
 import           Data.Char (isSpace)
 import           Data.List (sort, transpose)
--- import           Data.Tuple.Utils -- from stackage
+import           Data.List.Index (ifilter{-, imap-})
+-- import           Data.Maybe (catMaybes)
+import           Data.Tuple.Utils (snd3) -- from MissingH @ stackage
 
 -- ex1
 
@@ -12,11 +15,11 @@ import           Data.List (sort, transpose)
 -- doing `drop n xs` instead of `drop (n-1) xs` to save 4 chars :)
 --   which requires passing in 0-based `zip [0..]` instead of `zip [1..]`
 -- keeping the name `every` instead of a short one-character name, e.g., `e`
--- 73 chars
+-- 72 chars
 every :: (Int, [a]) -> [a]
 every (n, xs) = case drop n xs of
               (y:ys) -> y : every (n, ys)
-              []     -> []
+              _      -> []
 
 -- 85 chars of every implementation based on iterate and splitAt
 -- (\n -> (\l -> map last . takeWhile (not . null) . map fst . tail . iterate (splitAt n . snd) . ([],) $ l)) 2 [1,2,3,4,5,6]
@@ -47,10 +50,83 @@ skips :: [a] -> [[a]]
 --   https://kseo.github.io/posts/2016-12-24-reader-monad-and-ski-combinators.html
 -- skips = map every . zip [0..] . (flip replicate <*> length)
 -- skips = take . length <*> map every . zip [0..] . repeat
-
 -- seems to be the most readable
--- 44 + 73 = 117 chars
-skips l = map every . zip [0..] $ replicate (length l) l
+-- 44 + 72 = 116 chars
+--skips l = map every . zip [0..] $ replicate (length l) l
+
+-- utilizing list applicative to save calls to repeat
+-- and use a curried version of every to avoid zip
+-- everY = curry every
+-- 65 chars
+everY :: Int -> [a] -> [a]
+everY n xs = case drop n xs of
+              (y:ys) -> y : everY n ys
+              _      -> []
+-- reader applicative and list applicative
+-- skips = take . length <*> (\l -> everY <$> [0..] <*> [l])
+-- skips = take . length <*> ap (everY <$> [0..]) . pure
+-- 65 + 40 = 105 chars
+-- somehow non-point-free version is shorter due to [] vs pure
+--  and probably more intuitive due to <$> .. <*> .. vs ap (.. <$> ..)
+-- skips l = take (length l) $ everY <$> [0..] <*> [l]
+-- use liftA2 instead of .. <$> .. <*> ..
+--  still 1 char longer than non-point-free, but as intuitive now
+-- skips = take . length <*> liftA2 everY [0..] . pure
+-- 65 + 40 = 105 chars
+-- skips l = take (length l) $ liftA2 everY [0..] [l]
+-- 65 + 38 = 103 chars
+-- skips = liftA2 everY (map fst . zip [1..]) pure
+-- 65 + 37 = 102 chars
+-- skips l = liftA2 everY [0..(length l)-1] [l]
+-- 65 + 35 = 100 chars
+-- skips l = (flip everY) l <$> [1..length l]
+-- 65 + 40 = 105 chars
+-- skips = fmap . (flip sieve) <*> map fst . zip [1..]
+
+-- using ifilter from Data.List.Index to implement every
+-- every n = ifilter (\i _ -> (i+1) `mod` n == 0)
+-- 60 chars
+skips l = liftA2 (\n -> ifilter (\i _ -> mod (i+1) n == 0)) [1..length l] [l]
+-- 73 chars point free of l
+-- skips = liftA2 (liftA2 (\n -> ifilter (\i _ -> mod (i+1) n == 0)))  (map fst . zip [1..]) pure
+-- 70 chars point free in ifilter
+-- skips l = liftA2 (\n -> ifilter (const . ((==0) . (`mod` n) . (+1)))) [1..length l] [l]
+
+{-
+each column includes the value in rows that are factors of the current index,
+and then filter the empty cells in each row
+1234567890ABCDEF
+ 2 4 6 8 0 B D F
+  3  6  9  B  E
+   4   8   B   F
+    5    0    E
+     6     B
+      7      D
+       8       F
+-}
+{-
+-- 114 chars
+skips = takeWhile (not . null)
+      . map catMaybes
+      . transpose
+      . imap (\i -> imap
+                      -- (flip ((. ((==0) . ((i+1) `mod`) . (+1))) . bool Nothing ))
+                      (\j a -> (bool Nothing a (mod (i+1) (j+1) == 0)))
+                  . repeat . Just)
+-}
+{-
+-- 109 chars
+skips l = map catMaybes
+        . transpose
+        . imap (\i -> imap
+                        (\j a -> (bool Nothing a (mod (i+1) (j+1) == 0)))
+                    . replicate (length l) . Just)
+        $ l
+-}
+--skips l = liftA2 everY [0..(length l)-1] [l]
+--skips l = map every . zip [0..] $ replicate (length l) l
+-- 67 chars
+-- skips l = imap (\i -> ifilter (\j _ -> mod (j+1) (i+1) == 0)) $ replicate (length l) l
 
 
 -- ex2
@@ -59,10 +135,9 @@ localMaxima :: [Integer] -> [Integer]
 --localMaxima []    = []
 --localMaxima [_]   = []
 --localMaxima [_,_] = []
--- 78 chars
+-- 69 chars
 localMaxima =
-    -- snd3 from Data.Tuple.Utils is not from haskell-platform :|
-    map (\(_, x, _) -> x)
+    map snd3
     . filter (\(x, y, z) -> x < y && y > z)
     -- $ zip3 l (drop 1 l) (drop 2 $ l) l
     -- . (zip3 <$> id <*> drop 1 <*> drop 2)
@@ -73,7 +148,7 @@ localMaxima =
 histogram :: [Integer] -> String
 {-
 -- 147 chars
-histogram = 
+histogram =
     (++ "==========\n0123456789\n")
     . unlines
     -- . dropWhile (== "          ")
@@ -101,4 +176,8 @@ histogram =
         -- (\i -> map $ bool ' ' '*' . (==i))
         map . ((bool ' ' '*') .) . (==)
           <$> [0..9])
+    -- liftA2 version is 1 char longer
+    -- . liftA2
+    --    (map . ((bool ' ' '*') .) . (==))
+    --    [0..9]
     . pure
--- cis194/week3/zehua/test/ExercisesSpec.hs
+++ cis194/week3/zehua/test/ExercisesSpec.hs
@@ -17,6 +17,26 @@ spec = do
       skips ([1,2,3,4,5,6] :: [Int]) `shouldBe` [[1,2,3,4,5,6],[2,4,6],[3,6],[4],[5],[6]]
       skips [True,False] `shouldBe` [[True,False], [False]]
 
+    it "works with 16 items" $ do
+      skips "1234567890ABCDEF" `shouldBe` fmap (filter (/= ' '))
+          [ "1234567890ABCDEF"   -- 1
+          , " 2 4 6 8 0 B D F"   -- 2
+          , "  3  6  9  B  E "   -- 3
+          , "   4   8   B   F"   -- 4
+          , "    5    0    E "   -- 5
+          , "     6     B    "   -- 6
+          , "      7      D  "   -- 7
+          , "       8       F"   -- 8
+          , "        9       "   -- 9
+          , "         0      "   -- 10
+          , "          A     "   -- 11
+          , "           B    "   -- 12
+          , "            C   "   -- 13
+          , "             D  "   -- 14
+          , "              E "   -- 15
+          , "               F" ] -- 16
+
+
   describe "ex2" $ do
     it "returns empty given empty" $ do
       localMaxima ([] :: [Integer]) `shouldBe` []
