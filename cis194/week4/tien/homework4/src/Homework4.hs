d814ffee963d8b6339c9ce3ac6a1276eac3be4e9
Tien <g943942@gmail.com>
2019-09-06 23:49:26 +1000

    Week 4 Ex 2,3 (#58)

--- cis194/week4/tien/homework4/src/Homework4.hs
+++ cis194/week4/tien/homework4/src/Homework4.hs
@@ -30,3 +30,42 @@ data Tree a
   = Leaf
   | Node Integer (Tree a) a (Tree a)
   deriving (Show, Eq)
+
+checkDepth :: Eq a => Tree a -> Integer
+checkDepth Leaf = (0 :: Integer)
+checkDepth (Node _ Leaf _ Leaf) = 1
+checkDepth (Node _ Leaf _ right@(Node _ _ _ _)) = 1 + checkDepth right
+checkDepth (Node _ left@(Node _ _ _ _) _ Leaf) = 1 + checkDepth left
+checkDepth (Node _ left@(Node _ _ _ _) _ right@(Node _ _ _ _)) =
+  1 + checkDepth left + checkDepth right
+
+insert :: Eq t => t -> Tree t -> Tree t
+insert x Leaf = Node 0 Leaf x Leaf
+insert x (Node i left c Leaf) = Node i left c (Node (i + 1) Leaf x Leaf)
+insert x (Node i Leaf c right) = Node i (Node (i + 1) Leaf x Leaf) c right
+insert x (Node i left c right)
+  | (checkDepth left) > (checkDepth right) = Node i left c (insert x right)
+  | otherwise = Node i (insert x left) c right
+
+foldTree :: (Foldable t, Eq a) => t a -> Tree a
+foldTree = foldr insert Leaf
+
+-- Exercise 3.1 --
+xor :: [Bool] -> Bool
+xor = odd . foldr (\x acc -> acc + (bool 0 1 x)) (0 :: Integer)
+
+-- Exercise 3.2 --
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x acc -> f x : acc) []
+
+-- Exercise 3.3 --
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f base xs = foldr (\x acc -> f acc x) base (reverse xs)
+
+-- Exercise 4 --
+deletedSunDaram :: Integer -> [Integer]
+deletedSunDaram n =
+  [i + j + 2 * i * j | i <- [1 .. n], j <- [1 .. i], i + j + 2 * i * j < n]
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n = [2 * i + 1 | i <- [1 .. n], notElem i (deletedSunDaram n)]
--- cis194/week4/tien/homework4/test/Homework4Spec.hs
+++ cis194/week4/tien/homework4/test/Homework4Spec.hs
@@ -1,19 +1,45 @@
 import           Homework4
 import           Test.Hspec
 
-main::IO()
+main :: IO ()
 main =
   hspec $ do
-    describe "Homework4" $ do
+    describe "Homework4" $
       -- Exercise 1.1 --
+     do
       it "should return same results as fun1" $ do
-        let a = [7,3,2,5,6,2,8]
+        let a = [7, 3, 2, 5, 6, 2, 8]
         fun1' a `shouldBe` fun1 a
       -- Exercise 1.2 --
-      it "should return same results as fun2" $ do
+      it "should return same results as fun2" $
         -- cannot test 0 or minus numbers fun2 will not converge --
+       do
         fun2' 1 `shouldBe` fun2 1
         fun2' 2 `shouldBe` fun2 2
         fun2' 3 `shouldBe` fun2 3
         fun2' 4 `shouldBe` fun2 4
         fun2' 5 `shouldBe` fun2 5
+      -- Exercise 2 --
+      it "should return a balanced Tree" $ do
+        let (Node _ left _ right) = foldTree "ABCDEFGHIJ"
+        checkDepth left - checkDepth right `shouldSatisfy` (< 2)
+        let (Node _ left _ right) = foldTree "ABCDEFG"
+        checkDepth left `shouldBe` checkDepth right
+      -- Exercise 3.1 --
+      it "should return True if the number of True is odd" $ do
+        xor [False, True, False] `shouldBe` True
+        xor [False, True, False, False, True] `shouldBe` False
+      -- Exercise 3.2 --
+      it "should return the same result as map" $ do
+        let a = [2, 3, 4, 5]
+        map' (* 2) a `shouldBe` map (* 2) a
+      -- Exercise 3.3 --
+      it "should return the same result as foldl" $ do
+        let base = 0
+        let xs = [1, 3, 5]
+        foldl (-) base xs `shouldBe` myFoldl (-) base xs
+      -- Exercise 4 --
+      it "should return prime numbers up to 2n+2 given an integer n" $ do
+        sieveSundaram 5 `shouldBe` [3, 5, 7, 11]
+        sieveSundaram 30 `shouldBe`
+          [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61]
