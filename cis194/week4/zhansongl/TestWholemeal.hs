22ea6ac5fc420af58bd7e3e5cfb3f105504622d2
hvariant <lizhansong@hvariant.com>
2019-09-09 21:27:24 +1000

    Week4 (#70)

---
+++ cis194/week4/zhansongl/TestWholemeal.hs
@@ -0,0 +1,70 @@
+import Wholemeal
+import Test.Hspec
+import Test.QuickCheck
+
+main :: IO ()
+main = hspec $ do
+  describe "fun1'" $ do
+    it "should behave just like fun1" $ property $
+      \xs -> (fun1' xs) == (fun1 xs)
+
+  describe "fun2'" $ do
+    it "should behave just like fun2" $ property $
+      forAll (arbitrary :: Gen (Positive Integer))
+        (\x -> (fun2' $ getPositive x) == (fun2 $ getPositive x))
+
+  describe "foldTree" $ do
+    it "should produce balanced trees" $ property $
+      forAll (arbitrary :: Gen String)
+        (\s -> let t = foldTree s
+               in  balanced t && correctHeight t)
+
+  describe "xor" $ do
+    it "should return False for empty list" $ do
+      xor [] `shouldBe` False
+    it "should return correct value" $ do
+      xor [False, True, False] `shouldBe` True
+      xor [False, True, False, False, True] `shouldBe` False
+
+  describe "map'" $ do
+    it "should behave exactly like map" $ property $
+      forAll (arbitrary :: Gen [Integer])
+        (\xs -> map' (+2) xs `shouldBe` map (+2) xs)
+
+  describe "map'" $ do
+    it "should behave exactly like map" $ property $
+      forAll (arbitrary :: Gen [Integer])
+        (\xs -> map' (+2) xs `shouldBe` map (+2) xs)
+
+  describe "myFoldl" $ do
+    it "should behave exactly like foldl" $ property $
+      forAll (arbitrary :: Gen [String])
+        (\xs -> myFoldl (++) "__" xs `shouldBe` foldl (++) "__" xs)
+
+  describe "sieveSundaram" $ do
+    it "should return the correct prime numbers" $ do
+      sieveSundaram 10 `shouldBe` [3, 5, 7, 11, 13, 17, 19]
+      sieveSundaram 40 `shouldBe` [3, 5, 7, 11, 13, 17, 19,
+                                   23, 29, 31, 37, 41, 43,
+                                   47, 53, 59, 61, 67, 71,
+                                   73, 79]
+
+balanced :: Tree a -> Bool
+balanced Leaf = True
+balanced (Node h left _ right) =
+  (abs $ (getHeight left) - (getHeight right)) <= 1
+  && balanced left
+  && balanced right
+
+calcHeight :: Tree a -> Integer
+calcHeight Leaf = (-1)
+calcHeight (Node _ left _ right) =
+  1 + max (calcHeight left) (calcHeight right)
+
+correctHeight :: Tree a -> Bool
+correctHeight Leaf = True
+correctHeight t@(Node h left _ right) =
+  (calcHeight t) == h
+  && correctHeight left
+  && correctHeight right
+
---
+++ cis194/week4/zhansongl/Wholemeal.hs
@@ -0,0 +1,71 @@
+module Wholemeal where
+
+import Data.Bool (bool)
+import qualified Data.Array as A
+
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs)
+  | even x    = (x - 2) * fun1 xs
+  | otherwise = fun1 xs
+
+fun1' :: [Integer] -> Integer
+fun1' = foldr (*) 1 . map (flip (-) 2) . filter even
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n | even n    = n + fun2 (n `div` 2)
+       | otherwise = fun2 (3 * n + 1)
+
+fun2' :: Integer -> Integer
+fun2' = sum . filter even . takeWhile (/= 1) . iterate next
+  where next n = bool (3 * n + 1) (n `div` 2) $ even n
+
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+getHeight :: Tree a -> Integer
+getHeight Leaf = (-1)
+getHeight (Node h _ _ _) = h
+
+insert :: a -> Tree a -> Tree a
+insert x Leaf = Node 0 Leaf x Leaf
+insert x (Node h left y right)
+  | getHeight left < getHeight right =
+      let h' = 1 + max (getHeight left') (getHeight right)
+          left' = insert x left
+      in  Node h' left' y right
+  | otherwise =
+      let h' = 1 + max (getHeight left) (getHeight right')
+          right' = insert x right
+      in  Node h' left y right'
+
+foldTree :: [a] -> Tree a
+foldTree = foldr insert Leaf
+
+xor :: [Bool] -> Bool
+xor = odd . length . filter id
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x ys -> (f x):ys) []
+
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f base xs = foldr (\x g z -> g $ f z x) id xs $ base
+
+cartProd :: [a] -> [b] -> [(a, b)]
+cartProd xs ys = [(x,y) | x <- xs, y <- ys]
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n =
+  map (\x -> 2 * x + 1)
+  . map fst
+  . filter snd
+  . A.assocs
+  . (A.//) (A.listArray (1,n) (repeat True))
+  . flip zip (repeat False)
+  . filter (<= n) -- indices to delete
+  . map (\(i, j) -> i + j + 2 * i * j)
+  $ cartProd [1..(n `div` 2)] [1..(n `div` 2)]
+
