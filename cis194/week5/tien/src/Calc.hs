b355ea31b7af831daa9f329a1db3300f787dcc17
Tien <g943942@gmail.com>
2019-09-09 21:29:10 +1000

    Week5 Ex 1, 2, 3, 4, 5 (#67)

---
+++ cis194/week5/tien/src/Calc.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# OPTIONS_GHC -Wall #-}
+
+module Calc where
+
+import qualified ExprT   as ET
+import           Parser
+import qualified StackVM as SVM
+
+-- Exercise 1 --
+eval :: ET.ExprT -> Integer
+eval (ET.Lit a)   = a
+eval (ET.Add a b) = eval a + eval b
+eval (ET.Mul a b) = eval a * eval b
+
+-- Exercise 2 --
+evalStr :: String -> Maybe Integer
+evalStr = fmap eval . parseExp ET.Lit ET.Add ET.Mul
+
+-- Exercise 3 --
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ET.ExprT where
+  lit a = ET.Lit a
+  add a b = ET.Add a b
+  mul a b = ET.Mul a b
+
+-- Exercise 4 --
+newtype MinMax =
+  MinMax Integer
+  deriving (Eq, Show)
+
+newtype Mod7 =
+  Mod7 Integer
+  deriving (Eq, Show)
+
+instance Expr Integer where
+  lit a = a
+  add a b = a + b
+  mul a b = a * b
+
+instance Expr Bool where
+  lit = (> 0)
+  add = (||)
+  mul = (&&)
+
+instance Expr MinMax where
+  lit a = MinMax a
+  add (MinMax a) (MinMax b) = MinMax $ max a b
+  mul (MinMax a) (MinMax b) = MinMax $ min a b
+
+instance Expr Mod7 where
+  lit a = Mod7 $ mod a 7
+  add (Mod7 a) (Mod7 b) = Mod7 $ lit (a + b)
+  mul (Mod7 a) (Mod7 b) = Mod7 $ lit (a * b)
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger :: Maybe Integer
+testInteger = testExp
+
+testBool :: Maybe Bool
+testBool = testExp
+
+testMM :: Maybe MinMax
+testMM = testExp
+
+testSat :: Maybe Mod7
+testSat = testExp
+
+-- Exercise 5 --
+instance Expr SVM.Program where
+  lit a = [SVM.PushI a]
+  add a b = a ++ b ++ [SVM.Add]
+  mul a b = a ++ b ++ [SVM.Mul]
+
+compile :: String -> Maybe SVM.Program
+compile = parseExp lit add mul
---
+++ cis194/week5/tien/test/CalcSpec.hs
@@ -0,0 +1,30 @@
+import           Test.Hspec
+
+import           Calc
+import qualified ExprT      as ET
+import qualified StackVM    as SVM
+
+main :: IO ()
+main =
+  hspec $ do
+    describe "Calc" $
+      -- Exercise 1 --
+     do
+      it "should eval given equations" $ do
+        eval (ET.Mul (ET.Add (ET.Lit 2) (ET.Lit 3)) (ET.Lit 4)) `shouldBe` 20
+      -- Exercise 2 --
+      it "should eval given strings with Maybe wrapped" $ do
+        evalStr "(2+3)*4" `shouldBe` Just 20
+        evalStr "2+3*" `shouldBe` Nothing
+      -- -- Exercise 3 --
+      -- it "should have the same expression when using Expr typeclass" $ do
+      --   mul (add (lit 2) (lit 3)) (lit 4) :: ET.ExprT `shouldReturn` (ET.Mul (ET.Add (ET.Lit 2) (ET.Lit 3)) (ET.Lit 4))
+      -- Exercise 4 --
+      it "should do specific calculations based on the types" $ do
+        testInteger `shouldBe` Just (-7)
+        testBool `shouldBe` Just True
+        testMM `shouldBe` (Just $ MinMax 5)
+        testSat `shouldBe` (Just $ Mod7 14)
+      -- Exercise 5 --
+      it "should compile the given arithmetic string to Program" $ do
+        (show . compile  $ "(2+3)*4") `shouldBe` "Just [PushI 2,PushI 3,Add,PushI 4,Mul]"
