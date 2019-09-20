5a4b016c5aa385bdcce7edf456a8d8c56b7a888d
Tien <g943942@gmail.com>
2019-09-20 13:35:55 +1000

    Week 6 ex 1 - 4 (#95)

---
+++ cis194/week6/tien/src/Fibonacci.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+module Fibonacci where
+
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+-- exercise 1 --
+fibs1 :: [Integer]
+fibs1 = fib <$> [0 ..]
+
+-- exercise 2 --
+fibs2 :: [Integer]
+fibs2 = 0 : 1 : next fibs2
+  where
+    next (x:y:xs) = (x + y) : next (y : xs)
+
+-- exercise 3 --
+data Stream a =
+  Con a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Con x xs) = x : (streamToList xs)
+
+instance Show a => Show (Stream a) where
+  show = show . take 20 . streamToList
+
+-- exercise 4 --
+streamRepeat :: a -> Stream a
+streamRepeat = Con <*> streamRepeat
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Con x xs) = Con (f x) . streamMap f $ xs
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f x = Con x . streamFromSeed f . f $ x
+
+-- exercise 5 --
+nats :: Stream Integer
+nats = streamFromSeed (+1) 0
---
+++ cis194/week6/tien/test/FibonacciSpec.hs
@@ -0,0 +1,26 @@
+import           Fibonacci
+import           Test.Hspec
+
+main :: IO ()
+main =
+  hspec $ do
+    describe "Fibonacci" $ do
+      describe "exercise 1:" $ do
+        it "should return correct Fibonacci by given an Integer" $ do
+          fib 0 `shouldBe` 0
+          fib 1 `shouldBe` 1
+          fib 6 `shouldBe` 8
+        it "should generate an infinite Fibonacci sequence" $ do
+          take 10 fibs1 `shouldBe` [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+      describe "exercise 2:" $ do
+        it "should generate the same as fibs1 for first n elements with O(n)" $ do
+          take 10 fibs2 `shouldBe` take 10 fibs1
+      describe "exercise 3 & 4:" $ do
+        it "should show 20 elements of a stream composed of 5" $ do
+          show ( streamRepeat 5) `shouldBe`
+            "[5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]"
+        it "should show 20 elements of a stream composed of 6" $ do
+          show (streamMap  (+ 1) . streamRepeat $ 5) `shouldBe`
+            "[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6]"
+        it "should show 20 elements of a stream generated from seed 1 and unfold (*2)" $ do
+          show ( streamFromSeed (*2) 1) `shouldBe` "[1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288]"
