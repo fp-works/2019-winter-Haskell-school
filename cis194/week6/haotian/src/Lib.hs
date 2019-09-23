3eb076ecdca3c7f1b42998023f9446d77d03bc47
Robert Chang <cht8687@gmail.com>
2019-09-23 21:37:27 +1000

    Week6: 1-4 (#100)

---
+++ cis194/week6/haotian/src/Lib.hs
@@ -0,0 +1,34 @@
+module Lib where
+
+-- ex1
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+fibs1 :: [Integer]
+fibs1 = map fib [0..]
+
+-- https://stackoverflow.com/questions/1105765/generating-fibonacci-numbers-in-haskell
+-- ex2
+fibs2 :: [Integer]
+fibs2 = 0 : 1 : zipWith (+) fibs2 (tail fibs2)
+
+-- ex3
+data Stream a = Cons a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x xs) = x : streamToList xs 
+
+instance Show a => Show (Stream a) where
+  show = show . take 20 . streamToList
+
+-- ex4
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f x = Cons x (streamFromSeed f (f x))
---
+++ cis194/week6/haotian/test/LibSpec.hs
@@ -0,0 +1,22 @@
+module LibSpec where
+import Lib 
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Exercises 1" $ do
+    it "return correct data" $ do
+      take 10 fibs1 `shouldBe` [0,1,1,2,3,5,8,13,21,34]
+  describe "Exercises 2" $ do
+    it "return correct data" $ do
+      take 10 fibs2 `shouldBe` [0,1,1,2,3,5,8,13,21,34]
+  describe "Exercises 3" $ do
+    it "should return correct result" $ do
+      show (streamRepeat 1) `shouldBe`
+            "[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]"
+    it "should return correct result" $ do
+      show (streamMap (*2) . streamRepeat $ 1) `shouldBe`
+            "[2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]"
+    it "should return correct result" $ do
+      show (streamFromSeed (*2) 1) `shouldBe`
+            "[1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288]"
