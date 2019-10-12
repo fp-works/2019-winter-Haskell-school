2fb32455eebe668363589813b96a738cd96f3022
Steve Mao <maochenyan@gmail.com>
2019-10-12 20:10:56 +1100

    simplify dieMany

--- cis194/week12/stevemao/Risk.hs
+++ cis194/week12/stevemao/Risk.hs
@@ -30,28 +30,8 @@ type Army = Int
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
   deriving Show
 
-threeDies :: Rand StdGen [DieValue]
-threeDies =
-  die >>= \d1 ->
-  die >>= \d2 ->
-  die >>= \d3 ->
-  return . reverse . sort $ [d1,d2,d3]
-
-twoDies :: Rand StdGen [DieValue]
-twoDies =
-  die >>= \d1 ->
-  die >>= \d2 ->
-  return . reverse . sort $ [d1,d2]
-  
-oneDie :: Rand StdGen [DieValue]
-oneDie = 
-  die >>= \d ->
-  return [d]
-
 dieMany :: Int -> Rand StdGen [DieValue]
-dieMany 1 = oneDie
-dieMany 2 = twoDies
-dieMany _ = threeDies
+dieMany n = fmap (reverse . sort) . replicateM n $ die
 
 compareTwoLists :: [DieValue] -> [DieValue] -> [Bool]
 compareTwoLists (a : as) (d : ds) = (a > d) : compareTwoLists as ds
@@ -79,6 +59,7 @@ invade b@(Battlefield a _)
   | a < 2 = pure b
   | otherwise = battle b >>= invade
 
+-- TODO: make the simulator run in parallel
 successProb :: Battlefield -> Rand StdGen Double
 successProb b = calculateResult 0 0
   where calculateResult :: Double -> Double -> Rand StdGen Double
