3775896bc166e91007b8c6e37d4d9486042ee1c6
Steve Mao <maochenyan@gmail.com>
2019-10-15 17:07:48 +1100

    temp

--- cis194/week12/stevemao/Risk.hs
+++ cis194/week12/stevemao/Risk.hs
@@ -5,12 +5,14 @@ module Risk where
 
 import Control.Monad.Random
 import Data.List
+import Data.Universe.Helpers
+import Data.Tree
 
 ------------------------------------------------------------
 -- Die values
 
 newtype DieValue = DV { unDV :: Int } 
-  deriving (Eq, Ord, Show, Num)
+  deriving (Eq, Ord, Show, Num, Enum)
 
 first :: (a -> b) -> (a, c) -> (b, c)
 first f (a, c) = (f a, c)
@@ -29,9 +31,34 @@ type Army = Int
 
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
   deriving Show
+  
+-- data SteveTuple a b = SteveTuple (a, b)
+-- 
+-- instance Functor (SteveTuple a) where
+--   -- fmap f (SteveTuple (a, a')) = SteveTuple (f a, f a')
+-- 
+-- instance Applicative (SteveTuple a) where
+--   -- SteveTuple (a, a') <*> SteveTuple (b, b') = SteveTuple (a b, a' b')
+-- 
+-- instance Monad (SteveTuple a) where
+--   SteveTuple (a, a') >>= f = SteveTuple (a >>= f, a' >>= b')
+-- 
+-- bibind
 
+-- bibind :: (a1, m a) -> ((a1, a) -> ([a], m a)) -> m a
+-- bibind = undefined
+-- 
+-- foldBind :: Monad m => m a -> ([a], m a) -> Int -> ([a], m a)
+-- foldBind _ (as, m) 1 = (as, m)
+-- foldBind m' (as, m) n = bibind (as, m) newM
+--   where newM = \(as, r) -> (as ++ [r], m >>= )
+
+sortReverse :: Functor f => f [DieValue] -> f [DieValue]
+sortReverse = fmap (reverse . sort)
+
+-- TODO: replicateM is wrong. Use fold bind
 dieMany :: Int -> Rand StdGen [DieValue]
-dieMany n = fmap (reverse . sort) . replicateM n $ die
+dieMany n = sortReverse . replicateM n $ die
 
 count   :: Eq a => a -> [a] -> Int
 count x =  length . filter (==x)
@@ -63,3 +90,38 @@ successProb b = fmap (\bs -> (foldr f 0 bs) / fromIntegral simCount) . replicate
         f (Battlefield _ 0) acc = acc + 1
         f _ acc = acc
         simCount = 1000 :: Int
+
+cartprod :: [[a]] -> [[a]];
+cartprod [] = [[]]
+cartprod (xs:xss) = [x:ys | x<- xs, ys <-yss]
+                        where yss = cartprod xss
+
+diePlanned :: Int -> [[DieValue]]
+diePlanned n = sortReverse . cartprod . replicate n $ [1..6]
+
+battleAll :: Battlefield -> [Battlefield]
+battleAll (Battlefield a d) = do
+  let maxA = min (a - 1) 3
+  let maxD = min d 2
+
+  let attackss = diePlanned maxA -- [[1,1,1], [2,1,1]...]
+  let defendss = diePlanned maxD -- [[1,1], [2,1]...]
+
+  let allResults = if length (concat attackss) > 0 && length (concat defendss) > 0 then attackss +*+ defendss else [] -- [([1,1,1], [1,1]), ([2,1,1], [1,1])...]
+
+  fmap (\r -> Battlefield (a - count False r) $ d - count True r) $ fmap (\result -> uncurry (zipWith (>)) result) allResults
+
+buildTree :: Battlefield -> Tree Battlefield
+buildTree = unfoldTree f
+  where f :: Battlefield -> (Battlefield, [Battlefield])
+        f b = (b, battleAll b)
+
+calculateProb :: Tree Battlefield -> Double
+calculateProb = foldTree f
+  where f :: Battlefield -> [Double] -> Double
+        f (Battlefield _ 0) [] = 1
+        f _ [] = 0
+        f _ forest = sum forest / fromIntegral (length forest)
+
+exactSuccessProb :: Battlefield -> Double
+exactSuccessProb = calculateProb . buildTree
